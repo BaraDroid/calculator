d4c33e599a93de08e3daf945ad8699315c8c52f7
BaraDroid <lambeinovab@seznam.cz>
2025-07-20 12:13:24 +0200

    for loop for going through currentExample array for proocessing result

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
-// const digit0 = document.getElementById("digit0");
-// const digit1 = document.getElementById("digit1");
-// const digit2 = document.getElementById("digit2");
-// const digit3 = document.getElementById("digit3");
-// const digit4 = document.getElementById("digit4");
-// const digit5 = document.getElementById("digit5");
-// const digit6 = document.getElementById("digit6");
-// const digit7 = document.getElementById("digit7");
-// const digit8 = document.getElementById("digit8");
-// const digit9 = document.getElementById("digit9");
-//to nejak vubec nepotrebuju
 
-let currentDisplay = document.getElementById("currentDisplay")
+let currentDisplay = document.getElementById("currentDisplay");
+let accessMemory = [];
+let currentExample = [];
+let result = currentExample[0];
 
-function writeOperation(mathSign) {
-    currentDisplay.innerHTML += mathSign;
+function writeNumber(num) {
+    currentDisplay.innerHTML += num;
+    accessMemory.push(num);
+}
+
+function writeOperator(operator) {
+    getNewNumber();
+    currentDisplay.innerHTML += operator;
+    currentExample.push(operator);
+    console.log(currentExample);
 }
 
 function clearEntry() {
     currentDisplay.innerHTML = '';
+    //result = '';    //to bude tam, kde mam ted "hello"
+    currentExample = [];
+}
+
+function getNewNumber() {
+    let newNumber = accessMemory.join("");
+    accessMemory = [];
+    currentExample.push(parseInt(newNumber));
+        console.log(currentExample);
+
 }
 
 function processMathOperation() {
-    let mathOperation = currentDisplay.innerHTML;
-    console.log(typeof mathOperation);
-    mathOperation = +mathOperation;
-    console.log(typeof mathOperation);
+    console.log("ich prozesse");
+    getNewNumber();
+    let result = currentExample[0]; //nebo ho initializovat na zacatku jako nulu a pricitat
+    console.log("result in der forschleife", result);
+    for (let i = 0; i < currentExample.length; i+=2) {
+        
+        let operator = currentExample[i]; //if i is lichy cislo - protoze zacinam nulou
+        let nextNumber = currentExample[i+1]; //if i ist sudy cislo
+        
+        switch(operator) {
+            case "+":
+                result += nextNumber;
+                break;
+            case "-":
+                result -= nextNumber;
+                break;
+            case "x":
+                result *= nextNumber;
+                break;
+            case "/":
+                result /= nextNumber;
+                break;
+        }
+    console.log("my result", result);
 }
-
-//ted to musim u +-*/ rozdelit, tecka nerozdeluje, a udelat z toho cislo zvlast
-//pridat variable operator, ktery bude na +-atd. takze dve funkce, jedna pro psani
-//cisel a jedna pro operator
-//  switch (operator) {
-//         case '+': result = num1 + num2; break;
-//         case '-': result = num1 - num2; break;
-//         case '*': result = num1 * num2; break;
-//         case '/':
-//protoze je moznost mit vic cisel, nez 2, zwischenspeicher im Array
-//numbers[num1, num2] , zvlast je ulozit jako numbers(index)
-//ne, to bude lepsi to speichern jako operation["num1", "+", num2, "-", "etc"]
-//if not operator, parseInt, if operator, pridej znaminko
-//oder ich mache jestli se to da vynasobit jednickou a vyjde cislo nebo NaN
+}
